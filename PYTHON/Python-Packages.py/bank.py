7246b7c0a08ff7002ca62f350358c8eabd60c027
Dhathris45 <dhathris2004@gmail.com>
2026-03-30 17:02:45 +0530

    Add files via upload

---
+++ PYTHON/Python-Packages.py/bank.py
@@ -0,0 +1,4 @@
+import calculator
+
+print(calculator.add(1000, 500))
+print(calculator.multiply(200, 3))
---
+++ PYTHON/Python-Packages.py/trigno.py
@@ -0,0 +1,5 @@
+import calculator as c
+
+print(c.add(10, 20))
+print(c.multiply(5, 6))
+print(c.divide(20, 2))
